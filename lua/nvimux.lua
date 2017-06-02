a4f253b763a00d9fc32ae68164237ddf89297417
Henry Kupty <hkupty@gmail.com>
2017-06-02 14:48:26 -0300

    Readd log

--- lua/nvimux.lua
+++ lua/nvimux.lua
@@ -212,6 +212,7 @@ end
 nvimux.term.toggle = function()
   -- TODO Allow external commands
   local buf_nr = fns.variables.get{mode=vars.quickterm_scope, name='nvimux_last_buffer_id'}
+  print(buf_nr)
   if buf_nr == nil then
     nvimux.term.new_toggle()
   else
